df296df6eb6dc96540a1f19ef7c7e62acb7472b2
Adi <adi1.mitmpl2023@learner.manipal.edu>
2026-01-20 10:38:45 +0530

    Lab3 Report

--- PCAP/PCAPLab3/q4.c
+++ PCAP/PCAPLab3/q4.c
@@ -1,56 +1,59 @@
-//Program reads 2 strings of same length in root process. Using N processes including root process,
-//alternate each character of both strings and store in result string. Print result string in root process.
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <mpi.h>
+
 #define MAX 100
 
 int main(int argc, char *argv[]) {
     int rank, size, len, i;
-    char str1[MAX], str2[MAX], result[MAX * 2];
+    char str1[MAX], str2[MAX];
+    char *local_result, *final_result;
 
     MPI_Init(&argc, &argv);
     MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &size);
 
     if (rank == 0) {
-        // Read two strings from user
-        printf("Enter first string: ");
+        fprintf(stdout, "Enter first string: ");
+        fflush(stdout);
         scanf("%s", str1);
-        printf("Enter second string: ");
+
+        fprintf(stdout, "Enter second string: ");
+        fflush(stdout);
         scanf("%s", str2);
 
         len = strlen(str1);
         if (len != strlen(str2)) {
-            printf("Strings must be of the same length.\n");
+            fprintf(stderr, "Strings must be of same length\n");
             MPI_Abort(MPI_COMM_WORLD, 1);
         }
     }
 
-    // Broadcast length of strings to all processes
     MPI_Bcast(&len, 1, MPI_INT, 0, MPI_COMM_WORLD);
+    MPI_Bcast(str1, len + 1, MPI_CHAR, 0, MPI_COMM_WORLD);
+    MPI_Bcast(str2, len + 1, MPI_CHAR, 0, MPI_COMM_WORLD);
 
-    // Broadcast both strings to all processes
-    MPI_Bcast(str1, MAX, MPI_CHAR, 0, MPI_COMM_WORLD);
-    MPI_Bcast(str2, MAX, MPI_CHAR, 0, MPI_COMM_WORLD);
+    local_result = calloc(2 * len + 1, sizeof(char));
 
-    // Each process computes its part of the result
     for (i = rank; i < len; i += size) {
-        result[2 * i] = str1[i];
-        result[2 * i + 1] = str2[i];
+        local_result[2 * i]     = str1[i];
+        local_result[2 * i + 1] = str2[i];
     }
 
-    // Gather results at root process
-    MPI_Gather(&result[rank * 2], (len / size) * 2 + (rank < len % size ? 2 : 0), MPI_CHAR,
-               result, (len / size) * 2 + (rank < len % size ? 2 : 0), MPI_CHAR,
-               0, MPI_COMM_WORLD);
+    if (rank == 0)
+        final_result = calloc(2 * len + 1, sizeof(char));
+
+    MPI_Reduce(local_result, final_result,
+               2 * len + 1, MPI_CHAR,
+               MPI_MAX, 0, MPI_COMM_WORLD);
 
     if (rank == 0) {
-        result[2 * len] = '\0'; // Null-terminate the result string
-        printf("Resultant string: %s\n", result);
+        printf("Resultant string: %s\n", final_result);
+        free(final_result);
     }
 
+    free(local_result);
     MPI_Finalize();
     return 0;
 }
